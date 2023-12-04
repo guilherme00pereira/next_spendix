da594edcaecdc45a3ff0e5dd7bafb4c74b85a961
guilherme <guilherme00pereira@gmail.com>
2023-12-03 22:32:00 -0300

    v_0.1.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ThemeRegistry from "@/components/theme-registry/ThemeRegistry";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Spendix",
-  description: "Manage your expenses and income with Spendix",
+    title: "Spendix",
+    description: "Manage your expenses and income with Spendix",
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <body className={inter.className}>
-        <ThemeRegistry>
-          {children}
-        </ThemeRegistry>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <body className={inter.className}>
+            {children}
+        </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,44 +9,44 @@ import { Grid } from "@mui/material";
 export default function Home() {
   const theme = useTheme();
   return (
-    <Box
-      sx={{ minHeight: "100vh", backgroundColor: theme.palette.primary.light }}
-    >
-      <Grid
-        container
-        direction="column"
-        justifyContent="flex-end"
-        sx={{ minHeight: "100vh" }}
+      <Box
+          sx={{ minHeight: "100vh", backgroundColor: "white" }}
       >
-        <Grid item xs={12}>
-          <Grid
+        <Grid
             container
-            justifyContent="center"
-            alignItems="center"
-            sx={{ minHeight: "calc(100vh - 68px)" }}
-          >
-            <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
-              <Box
-                sx={{
-                  maxWidth: { xs: 400, lg: 475 },
-                  margin: { xs: 2.5, md: 3 },
-                  "& > *": {
-                    flexGrow: 1,
-                    flexBasis: "50%",
-                  },
-                }}
-              >
-                <Link href="/dashboard" passHref>
-                  <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="secondary">
-                    Sign in
-                  </Button>
-                </Link>
-              
-              </Box>
+            direction="column"
+            justifyContent="flex-end"
+            sx={{ minHeight: "100vh" }}
+        >
+          <Grid item xs={12}>
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                sx={{ minHeight: "calc(100vh - 68px)" }}
+            >
+              <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                <Box
+                    sx={{
+                      maxWidth: { xs: 400, lg: 475 },
+                      margin: { xs: 2.5, md: 3 },
+                      "& > *": {
+                        flexGrow: 1,
+                        flexBasis: "50%",
+                      },
+                    }}
+                >
+                  <Link href="/dashboard" passHref>
+                    <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="secondary">
+                      Sign in
+                    </Button>
+                  </Link>
+
+                </Box>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
   );
 }
